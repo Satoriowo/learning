ca96fd80b263df1a0f9914ec6bc5cbe9bcdfb617
Satoriowo <valeriana4151@gmail.com>
2023-06-13 22:26:44 -0600

    string methods?

---
+++ 2. Python/strm.py
@@ -0,0 +1,12 @@
+name = "Newt"
+
+print(len(name))
+print(name.find("e"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("N"))
+print(name.replace("e","o"))
+print(name*2)
