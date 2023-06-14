6dcbcb956c4ae2b4218e3aec0f05ac1088d78833
Satoriowo <valeriana4151@gmail.com>
2023-06-13 22:19:14 -0600

    assign multiple variables in one line code

---
+++ 2. Python/mulvar.py
@@ -0,0 +1,23 @@
+# multiple assignment : allow us to assign multiple variables at the same time in one line code
+
+#name = "Newt"
+#age = 18
+#attractive = True
+
+name, age, attractive = "Newt", 18, True
+
+print(name)
+print(age)
+print(attractive)
+
+#Arturo = 19
+#Daniel = 19
+#Cesar = 19
+#Mariana = 19
+
+Arturo = Daniel = Cesar = Mariana = 30
+
+print(Arturo)
+print(Daniel)
+print(Cesar)
+print(Mariana)
