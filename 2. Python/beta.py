54f2dc26f2362edd31d6f48fb84bc993e3c224f3
Satoriowo <valeriana4151@gmail.com>
2023-06-13 19:49:25 -0600

    se agrego python

---
+++ 2. Python/beta.py
@@ -0,0 +1,11 @@
+#variable - container for a value
+
+name = 'bro' # Its a string
+
+first_name = 'Katherine'
+last_name = 'Hernández'
+full_name = first_name + ' ' + last_name
+
+print(full_name)
+
+
