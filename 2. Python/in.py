aec4f0a4b722f75cb27da18586695df4035c1633
Satoriowo <valeriana4151@gmail.com>
2023-06-14 19:20:12 -0600

    input file

---
+++ 2. Python/in.py
@@ -0,0 +1,7 @@
+name = (input("What's your name?: "))
+age = int(input("What's your age?: "))
+height = float(input("What's your height?: "))
+
+print("So your name is " +name)
+print("You are " + str(age) + " years old")
+print("and then ,your height is " + str(height) + " cm")
