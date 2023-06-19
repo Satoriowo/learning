bec253056ace0b354c94677d0dc1c7c0a86eaf84
Katherine/Newt Hernández Dávila <valeriana4151@gmail.com>
2023-06-19 17:13:38 -0600

    math functions

--- 2. Python/in.py
+++ 2. Python/in.py
@@ -1,6 +1,6 @@
-name = (input("What's your name?: "))
-age = int(input("What's your age?: "))
-height = float(input("What's your height?: "))
+name = (input("What's your name?: ")) #solo usamos input pq automaticamente es un valor str
+age = int(input("What's your age?: ")) #we use int(input()) para que el valor que reciba sea un int y es lo que ocupamos
+height = float(input("What's your height?: ")) # float(input()) para que el valor recibido sea el de float
 
 print("So your name is " +name)
 print("You are " + str(age) + " years old")
---
+++ 2. Python/mathf.py
@@ -0,0 +1,16 @@
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+#print(round(pi)) #rounded a number
+#print(math.ceil(pi)) # rounded to a number up
+#print(math.floor(pi)) #rounded to a number down
+print(abs(pi)) #absolute value
+print(pow(pi,2)) #la potencia de pi a la 2
+print(math.sqrt(pi)) #la raíz de pi
+print(max(x,y,z)) #el número mayor de los que estan  ()
+print(min(x,y,z)) # el número menor de los que estan en ()
+
