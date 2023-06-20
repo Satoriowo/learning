7917d24b3ef73447a6406cdf27dbbd2d3630ffca
Katherine/Newt Hernández Dávila <valeriana4151@gmail.com>
2023-06-20 00:03:55 -0600

    break continue or pass

---
+++ 2. Python/locon.py
@@ -0,0 +1,24 @@
+# loop control statements = change a loops execution from its normal sequence
+
+# break =    used to terminate the loop entirely
+# continue = skips to the next iteration of the loop
+# pass =     does nothing, act as a placeholder
+
+#while True:
+#    name = input("Enter your name: ")
+#    if name != "":
+#        break
+
+#phone_number = "91-1415-9845"
+
+#for i in phone_number:
+#    if i == "-":
+#        continue
+#    print(i, end="")
+
+for i in range (1,21): # de 1 a 21
+
+    if i == 13:
+        pass #does nothing
+    else: 
+        print(i) 
