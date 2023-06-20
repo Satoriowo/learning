cb08b15bfde43a43f90cdcf500a5adeba2c06065
Katherine/Newt Hernández Dávila <valeriana4151@gmail.com>
2023-06-19 20:57:58 -0600

    if statements

---
+++ 2. Python/if.py
@@ -0,0 +1,12 @@
+# if statement = block of code that will execute if it's condition is true
+
+age = int(input('How old are you?: '))
+
+if age == 100:
+    print("You are too old, like a century!")
+elif age >= 18:
+    print('You are an adult, sad :c')
+elif age < 0:
+    print("You haven't born yet dudee!")
+else : 
+    print('You are a little kid! :3')
