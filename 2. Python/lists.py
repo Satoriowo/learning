072bd26b64c59412b300873b8aa54d9afbe3499d
Katherine/Newt Hernández Dávila <valeriana4151@gmail.com>
2023-06-20 00:17:08 -0600

    lists

---
+++ 2. Python/lists.py
@@ -0,0 +1,23 @@
+# list = used to store multiple items in a single variable
+
+food = ['pizza', 'hamburguer', 'hotdog', 'spaghetti', 'pudding']
+#          0           1           2           3        4
+
+food[0] = "sushi"
+
+#food.append("ice cream") se agrega lo de ("") al final
+
+#food.remove("hamburguer") la primera recurrencia q encuentre
+
+#food.pop() el último elemento en la lista
+
+#food.insert(0,'cake') inserta ese elemento en el lugar indicado
+
+#food.sort() las ordena(?)
+
+#food.clear()
+
+#print(food[0])
+
+for x in food: 
+    print(x)
