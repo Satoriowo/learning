80798b3bd6e115813329dcd39421b44318f8ecb1
Katherine/Newt Hernández Dávila <valeriana4151@gmail.com>
2023-06-20 17:45:02 -0600

    2D list

---
+++ 2. Python/2dlist.py
@@ -0,0 +1,16 @@
+# 2d lists = a list of lists
+
+# listas para food
+drinks = ['coffee','soda','tea']
+dinner = ['pizza','hamburger','hotdog']
+dessert = ['cake','ice cream']
+
+food = [drinks,dinner,dessert] # recopila las 3 listas anteriores
+
+#print(food[0]) # imprime la primer lista completa
+#print(food[0][1]) # imprime el 2 elemento de la 1° lista
+
+print(food[1][1]) # el primer [] maneja la lista
+#                   el 2do [] maneja el elemento de la lista q se imprimira
+
+
