a120dd7ef7b91832543cfc7befab0a52b5624a04
Katherine/Newt Hernández Dávila <valeriana4151@gmail.com>
2023-06-21 15:38:07 -0600

    indexing

--- 2. Python/ind.py
+++ 2. Python/ind.py
@@ -2,7 +2,13 @@
 
 name = 'newt Study'
 
-if(name[0].islower()):
-    name = name.capitalize()
+#if(name[0].islower()): # if the first letter in name is lower, then
+#    name = name.capitalize() # if that's true, capitalize the first letter
 
-print(name)
+first_name = name[0:4].upper()
+last_name = name[5:].lower()
+last_character = name[-1]
+
+print(first_name) 
+print(last_name)
+print(last_character)
