02e993f5da0bbee6470d009b842afc73bc0e04c8
Katherine/Newt Hernández Dávila <valeriana4151@gmail.com>
2023-06-20 20:08:24 -0600

    tuples

--- 2. Python/tuplesp.py
+++ 2. Python/tuplesp.py
@@ -3,6 +3,12 @@
 
 student = ("Newt",18,"Male")
 
-print(student.count("Newt"))
+print(student.count("Newt")) # how many times value appears
 
+print(student.index("Male")) # find the index of the value, the position
 
+for x in student: # for to print all the values in students
+    print(x)      
+
+if "Newt" in student:       # if the value "" is in the var student then print that
+    print("Newt is here!")
