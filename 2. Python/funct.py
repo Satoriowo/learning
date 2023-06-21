2e6bcb73f35301b8b58ff4c0b8976753c865f55e
Katherine/Newt Hernández Dávila <valeriana4151@gmail.com>
2023-06-21 16:56:52 -0600

    functions

---
+++ 2. Python/funct.py
@@ -0,0 +1,18 @@
+# function = a block of code which is executed only when it's called.
+
+def hello(name): # the there will be : def hello(first_name,last_name,age)
+    print("Hello! "+name) # there print('Hello! ' + first_name+' '+last_name)
+    #also this: print('You are '+str(age)+' years old')
+    print("Have a nice day!")
+
+# hello("Newt")
+# or
+# name = "Newt"
+# hello(name)
+# or
+# my_name = "Newt"
+# hello(my_name)
+# or 
+# hello('Newt','Quispe',18) if this
+
+hello('Newt')
