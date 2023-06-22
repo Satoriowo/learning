94a63d1cadb69e7764ff005bf33a2489f56d2b9d
Katherine/Newt Hernández Dávila <valeriana4151@gmail.com>
2023-06-21 18:52:59 -0600

    return statement

---
+++ 2. Python/retu.py
@@ -0,0 +1,15 @@
+# return statement = function send Python values/objects back to the caller.
+#                    These values/objects are known as the function's return value
+
+def multiply(number1,number2):
+    result = number1 * number2
+    return result
+
+print(multiply(3,4))
+
+def suma(num1,num2):
+    return num1 + num2
+
+x = suma(2,3)
+
+print(x)
